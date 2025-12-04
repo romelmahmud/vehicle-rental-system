01d8fea69b25dbe6dde3caed425fb1c062bfd48c
romel <romelmahmud.dev@gmail.com>
2025-12-04 20:13:47 +0600

    spit app code to app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
-import express from "express";
+import app from "./app";
 import { config } from "./config";
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("Server is up and running!");
-});
 
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);

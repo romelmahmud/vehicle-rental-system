75f1dc7ea8055ae6c96444f8657366cfceaf3b68
romel <romelmahmud.dev@gmail.com>
2025-12-06 23:59:34 +0600

    vehicle database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -18,6 +18,19 @@ const initDB = async () => {
     updated_at TIMESTAMP DEFAULT NOW()
   )
   `);
-};
 
+  await pool.query(`
+  CREATE TABLE IF NOT EXISTS vehicles(
+    id SERIAL PRIMARY KEY,
+    vehicle_name VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+    registration_number VARCHAR(50) UNIQUE NOT NULL,
+    daily_rent_price NUMERIC NOT NULL CHECK (daily_rent_price > 0),
+    availability_status VARCHAR(20) NOT NULL DEFAULT 'available' 
+      CHECK (availability_status IN ('available', 'booked')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`);
+};
 export default initDB;
